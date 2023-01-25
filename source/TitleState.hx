7dce6e2fc15d6869f6cc6c8b42f73685e23acbb3
MagnumPoland <t.bajorowy@gmail.com>
2023-01-25 21:12:51 +0100

    another quick fix
    eya

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -315,6 +315,7 @@ class TitleState extends MusicBeatState
 				gfDance.animation.addByIndices('danceRight', 'BB Title Bump', [27, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], "", 24, false);
 			case 'SOMETHINGVERYOLD':
 				logoBl.frames = Paths.getSparrowAtlas('kyeeOLDLOLSOBADXD');
+				logoBl.animation.addByIndices('bump', 'logo bumpin', [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14], "", 24, false);
 			#end
 
 			default:
